ef96c56781256fa0f5d32904758ef658fdd9f0c1
Dave Sullivan <dave@davesullivan.ca>
2023-04-03 07:40:16 -0700

    disable a bunch of plugins

--- app/assets/javascripts/administrate-field-froala/application.js
+++ app/assets/javascripts/administrate-field-froala/application.js
@@ -1,15 +1,7 @@
 //= require froala_editor.min.js
-//= require plugins/code_beautifier.min.js
 //= require plugins/code_view.min.js
-//= require plugins/colors.min.js
-//= require plugins/font_family.min.js
-//= require plugins/font_size.min.js
-//= require plugins/inline_class.min.js
-//= require plugins/inline_style.min.js
 //= require plugins/link.min.js
 //= require plugins/lists.min.js
-//= require plugins/paragraph_format.min.js
-//= require plugins/paragraph_style.min.js
 //= require plugins/table.min.js
 
 
